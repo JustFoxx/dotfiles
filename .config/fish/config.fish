1778faf2ea6ecfd60696473763e5ad13b361c98d
JustFoxx <kittech@skiff.com>
2024-02-16 19:04:38 +0000

    build aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,8 @@
 fish_add_path ~/.cargo/bin/
+set -x CFLAGS "-march=native -O3 -pipe -fno-plt -fexceptions -Wp,-D_FORTIFY_SOURCE=2 -Wformat -Werror=format-security -fstack-clash-protection"
+set -x CXXFLAGS "$CFLAGS -Wp,-D_GLIBCXX_ASSERTIONS"
 set -x RUSTFLAGS "-C opt-level=3 -C target_cpu=native"
+set -x MAKEFLAGS "-j16"
 alias eza="eza -Ga --hyperlink --color always --icons always"
 alias ls="eza"
 alias cat="bat"
